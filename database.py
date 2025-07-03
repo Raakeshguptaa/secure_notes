940ef7edf8a6bda1ddb20f70afcd8627eb5214cf
raakeshguptaa <rakesh.works20@gmail.com>
2025-07-04 01:39:25 +0530

    base = declaritive() added

--- database.py
+++ database.py
@@ -1,5 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,relationship,Session
+from sqlalchemy.ext.declarative import declarative_base
+
+
 engine = create_engine("postgres://postgres:rakeshpostgres@localhost/secure_notes", echo=True)
 
 Sessionlocal = sessionmaker(bind=engine)
+base = declarative_base()
